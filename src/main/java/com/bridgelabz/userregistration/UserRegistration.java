c0ced397032665681c20b3a3bd9d1a78975c978b
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-18 11:27:48 +0530

    Added : UC2_validate_last_name

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -3,15 +3,29 @@ package com.bridgelabz.userregistration;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    //UC1 : First name starts with Cap and has minimum 3 characters
+
+    //Method to Validate First name
     public boolean validateFirstName(String fName)
     {
-        String regex = "[A-Z][a-z]{3,}";
+        String regex = "[A-Z][a-z]{2,}";
         Pattern pattern=Pattern.compile(regex);
         boolean check=pattern.matcher(fName).matches();
         return check;
     }
+
+    // UC2 :  Last name starts with Cap and has minimum 3 characters
+
+    // Method to validate last name
+    public boolean validateLastName(String lName) {
+        String regex = "[A-Z][a-z]{2,}";  // Minimum 3 characters in total
+        Pattern pattern = Pattern.compile(regex);
+        boolean check = pattern.matcher(lName).matches();
+        return check;
+    }
     public static void main(String[] args) {
         UserRegistration userRegistration=new UserRegistration();
-        System.out.println(userRegistration.validateFirstName("john"));
+        System.out.println(userRegistration.validateFirstName("Nivrutti"));
+        System.out.println(userRegistration.validateLastName("Wagh"));
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -5,17 +5,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
+    // Test case for correct first name
     @Test
     public void FirstNameCorrectTest()
     {
         UserRegistration user=new UserRegistration();
-        boolean result=user.validateFirstName("John");
+        boolean result=user.validateFirstName("Nivrutti");
         Assertions.assertTrue(result);
     }
+
+    // Test case for incorrect first name
     @Test
     public void FirstNameIncorrectTest() {
         UserRegistration user=new UserRegistration();
-        boolean result=user.validateFirstName("john");
+        boolean result=user.validateFirstName("nivrutti");
+        Assertions.assertFalse(result);
+    }
+
+    // Test case for correct last name
+    @Test
+    public void LastNameCorrectTest() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateLastName("Wagh");
+        Assertions.assertTrue(result);
+    }
+
+    // Test case for incorrect last name
+    @Test
+    public void LastNameIncorrectTest() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateLastName("wagh");
         Assertions.assertFalse(result);
     }
 }
